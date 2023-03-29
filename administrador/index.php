9cc21f9b088c5533a8ab04c67ca7bb9258c31704
Danny-da <896547@senati.pe>
2023-03-29 12:40:44 -0500

    avance y carpetas creadas

--- administrador/index.php
+++ administrador/index.php
@@ -1,3 +1,8 @@
+<?php
+if($_POST){
+    header('Location:inicio.php');
+}
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,18 +17,36 @@
   <body>
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-4">
+<br><br><br>
+                </div>
+                <div class="col-md-4">
                     <div class="card">
                         <div class="card-header">
-                            Header
+                            Login
                         </div>
                         <div class="card-body">
-                            <h4 class="card-title">Title</h4>
-                            <p class="card-text">Text</p>
+                            <form method = "POST">
+                            <div class = "form-group">
+
+                            <label for="exampleInputEmail1">Usuario</label>
+
+                            <input type="text" class="form-control" name="usuario" placeholder="Escribe tu usuario">
+
+                            </div>
+                            <div class="form-group">
+
+                            <label for="exampleInputPassword1">Contraseña:</label>
+
+                            <input type="password" class="form-control" name="contraseña" placeholder="Escribe tu contraseña">
+
+                            </div>
+                            <button type="submit" class="btn btn-primary">Entrar al Administrador</button>
+                            </form>
+                            
+                            
                         </div>
-                        <div class="card-footer text-muted">
-                            Footer
-                        </div>
+                        
                     </div>
                 </div>
                 
@@ -37,8 +60,8 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
   </body>
 </html>
